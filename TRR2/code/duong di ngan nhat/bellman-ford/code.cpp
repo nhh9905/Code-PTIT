b5fc99a53cfff3acf85d7879e36ef9fa964d4dbc
nhh9905 <hoanglclvt@gmail.com>
2025-06-21 23:15:02 +0700

    On_tap_TRR2

--- TRR2/code/duong di ngan nhat/bellman-ford/code.cpp
+++ TRR2/code/duong di ngan nhat/bellman-ford/code.cpp
@@ -6,45 +6,54 @@ struct edge {
     int x, y, w;
 };
 int n, m;
-vector<edge> dscanh;
-int d[1005];
+vector<edge> edges;
+int d[1005], par[1005];
 
-void nhap() {
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        int x, y, w;
-        cin >> x >> y >> w;
-        dscanh.push_back({x, y, w});
-    }
-}
-
-void Bellman_ford(int s) {
-    // gan nhan duong di
-    // cac dinh khong phai dinh goc deu co chi phi duong di la vo cung
-    // dinh goc co chi phi duong di la 0
-    for (int i = 0; i <= n; i++)
-        d[i] = 1e9;
+void Bellman_Ford(int s) {
+    for (int i = 1; i <= n; i++)
+        d[i] = INT_MAX;
     d[s] = 0;
 
-    // buoc lặp n - 1
-    for (int i = 0; i < n - 1; i++) {
-        // duyệt tất cả các cạnh trên đồ thị
-        // relaxation
-        for (int j = 0; j < m; j++) {
-            edge tmp = dscanh[j];
-            if (d[tmp.x] < 1e9)
-                d[tmp.y] = min(d[tmp.y], d[tmp.x] + tmp.w);
+    for (int i = 1; i < n; i++) {
+        for (auto [u, v, w]:edges) {
+            if (d[u] != INT_MAX && d[v] > d[u] + w) {
+                d[v] = d[u] + w;
+                par[v] = u;
+            }
         }
     }
-    for (int i = 1; i <= n; i++)
-        cout << d[i] << " ";
+
+    for (int i = 1; i <= n; i++) {
+        cout << d[i] << ": ";
+
+        int t = i;
+        vector<int> vec;
+        while (t != s) {
+            vec.push_back(t);
+            t = par[t];
+        }
+        vec.push_back(s);
+
+        reverse(begin(vec), end(vec));
+
+        for (int x:vec)
+            cout << x << " ";
+        cout << endl;
+    }
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
+    freopen("a.inp", "r", stdin);
 
-    nhap();
-    Bellman_ford(1);
+    cin >> n >> m;
+    while (m--) {
+        int x, y, w;
+        cin >> x >> y >> w;
+        edges.push_back({x, y, w});
+    }
+
+    Bellman_Ford(1);
     return 0;
 }
